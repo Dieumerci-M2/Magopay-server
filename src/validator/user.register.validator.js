acf30d9e08b7d8284536a639fcb247c5bdc9d44a
JospinKanane <jospinkanane@yahoo.com>
2023-07-03 12:28:35 +0200

    update and upgrade login and signup

--- src/validator/user.register.validator.js
+++ src/validator/user.register.validator.js
@@ -7,19 +7,4 @@ const registerValidator = joi.object().keys({
 
 });
 
-// const userValidation = async (req, res, next) => {
-// 	const payload = {
-// 		name: req.body.name,
-// 		email: req.body.email,
-// 		password: req.body.password,
-//     }
-
-//     const error  = data.validate(payload);
-//     if (error) {
-//         res.status(406);
-//         return res.json({message : "Les données que vous avez entré sont pas correct !"});
-//     }
-//     next();
-// };
-
 export default registerValidator
